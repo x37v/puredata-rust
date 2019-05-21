e787372c076d776fb683121613354af5fbafaf9b
Alex Norman <alex@x37v.info>
2019-05-21 07:44:10 -0700

    remove external from lib

--- external/src/wrapper.rs
+++ external/src/wrapper.rs
@@ -1,11 +1,6 @@
 use crate::builder::{Builder, ExternalBuilder};
 use crate::external::External;
 use crate::obj::AsObject;
-use crate::outlet::{Outlet, OutletSend, OutletType};
-
-use std::ffi::CString;
-use std::ops::Deref;
-use std::rc::Rc;
 
 #[repr(C)]
 pub struct ExternalWrapper<T>
@@ -13,7 +8,7 @@ where
     T: External,
 {
     x_obj: puredata_sys::t_object,
-    external: Option<T>,
+    pub external: Option<T>, //XXX TMP
 }
 
 impl<T> ExternalWrapper<T>
@@ -34,68 +29,4 @@ where
     fn as_obj(&mut self) -> *mut puredata_sys::t_object {
         &mut self.x_obj
     }
-}
-
-pub type Wrapped = ExternalWrapper<HelloWorldExternal>;
-
-static mut HELLOWORLD_CLASS: Option<*mut puredata_sys::_class> = None;
-
-pub struct HelloWorldExternal {
-    inlet: Rc<dyn Deref<Target = puredata_sys::t_float>>,
-    outlet: Rc<dyn OutletSend>,
-}
-
-impl External for HelloWorldExternal {
-    fn new(builder: &mut dyn ExternalBuilder<Self>) -> Self {
-        Self {
-            inlet: builder.new_passive_float_inlet(4f32),
-            outlet: builder.new_outlet(OutletType::Float),
-        }
-    }
-}
-
-impl HelloWorldExternal {
-    pub fn bang(&mut self) {
-        unsafe {
-            let m =
-                CString::new(format!("{}", **self.inlet).to_string()).expect("CString::new failed");
-            puredata_sys::post(m.as_ptr());
-        }
-    }
-}
-
-pub unsafe extern "C" fn helloworld_new() -> *mut ::std::os::raw::c_void {
-    let obj = std::mem::transmute::<*mut puredata_sys::t_pd, &mut Wrapped>(puredata_sys::pd_new(
-        HELLOWORLD_CLASS.unwrap(),
-    ));
-    obj.init();
-    obj as *mut Wrapped as *mut ::std::os::raw::c_void
-}
-
-pub unsafe extern "C" fn helloworld_bang(x: &mut Wrapped) {
-    //XXX sketchy, but works
-    if let Some(e) = &mut x.external {
-        e.bang();
-    }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn helloworld_setup() {
-    let name = CString::new("helloworld").expect("CString::new failed");
-    let c = puredata_sys::class_new(
-        puredata_sys::gensym(name.as_ptr()),
-        Some(helloworld_new),
-        None,
-        std::mem::size_of::<Wrapped>(),
-        0,
-        0,
-    );
-    HELLOWORLD_CLASS = Some(c);
-    puredata_sys::class_addbang(
-        c,
-        Some(std::mem::transmute::<
-            unsafe extern "C" fn(&mut Wrapped),
-            unsafe extern "C" fn(),
-        >(helloworld_bang)),
-    );
 }
--- external/test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cargo build && mv target/debug/libhelloworld.so helloworld.pd_linux && pd test.pd
