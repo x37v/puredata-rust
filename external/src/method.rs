a4f273c95b8f0449b9d64ba62c3ac4078c318b07
Alex Norman <xnor@perfectco.com>
2019-05-24 09:44:23 -0700

    identifying defaults with a number

--- external/src/method.rs
+++ external/src/method.rs
@@ -6,6 +6,7 @@ pub type F3<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_float, pured
 pub type F4<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float);
 pub type F5<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float);
 pub type F6<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float, puredata_sys::t_float);
+
 pub type S1<T> = fn (&mut T, puredata_sys::t_symbol);
 pub type S2<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_symbol);
 pub type S3<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol);
@@ -13,39 +14,32 @@ pub type S4<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_symbol, pur
 pub type S5<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol);
 pub type S6<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol, puredata_sys::t_symbol);
 
+pub type FS<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_symbol);
+pub type SF<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_float);
+pub type FSF<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_symbol, puredata_sys::t_float);
+pub type SFF<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_float, puredata_sys::t_float);
+pub type FSS<T> = fn (&mut T, puredata_sys::t_float, puredata_sys::t_symbol, puredata_sys::t_symbol);
+pub type SFS<T> = fn (&mut T, puredata_sys::t_symbol, puredata_sys::t_float, puredata_sys::t_symbol);
+
+
 pub enum Method<T> {
     Bang(fn(&mut T)),
     Float(F1<T>),
     Sel(CString, fn(&mut T)),
 
-    SelF(CString, F1<T>),
-    SelDF(CString, F1<T>),
+    //selector, func, number of defaults (from back, at most the arity of the func)
+    SelF(CString, F1<T>, usize),
+    SelFF(CString, F2<T>, usize),
+    SelFFF(CString, F3<T>, usize),
+    SelFFFF(CString, F4<T>, usize),
+    SelFFFFF(CString, F5<T>, usize),
+    SelFFFFFF(CString, F6<T>, usize),
 
-    SelFF(CString, F2<T>),
-    SelFDF(CString, F2<T>),
-
-    SelFFF(CString, F3<T>),
-    SelFFDF(CString, F3<T>),
-    SelFDFDF(CString, F3<T>),
-
-    SelFFFF(CString, F4<T>),
-    SelFFFDF(CString, F4<T>),
-    SelFFDFDF(CString, F4<T>),
-    SelFDFDFDF(CString, F4<T>),
-    SelDFDFDFDF(CString, F4<T>),
-
-    SelFFFFF(CString, F5<T>),
-    SelFFFFDF(CString, F5<T>),
-    SelFFFDFDF(CString, F5<T>),
-    SelFFDFDFDF(CString, F5<T>),
-    SelFDFDFDFDF(CString, F5<T>),
-    SelDFDFDFDFDF(CString, F5<T>),
-
-    SelFFFFFF(CString, F6<T>),
-    SelFFFFFDF(CString, F6<T>),
-    SelFFFFDFDF(CString, F6<T>),
-    SelFFFDFDFDF(CString, F6<T>),
-    SelFFDFDFDFDF(CString, F6<T>),
-    SelFDFDFDFDFDF(CString, F6<T>),
-    SelDFDFDFDFDFDF(CString, F6<T>),
+    //selector, func, number of defaults (from back, at most the arity of the func)
+    SelS(CString, S1<T>, usize),
+    SelSS(CString, S2<T>, usize),
+    SelSSS(CString, S3<T>, usize),
+    SelSSSS(CString, S4<T>, usize),
+    SelSSSSS(CString, S5<T>, usize),
+    SelSSSSSS(CString, S6<T>, usize),
 }
