fa097957da508ca6cb498ff2a222da48b463eb44
Alex Norman <alex@x37v.info>
2019-05-30 08:14:20 -0700

    panic if other methods are called

--- external/src/class.rs
+++ external/src/class.rs
@@ -47,7 +47,7 @@ impl<T> Class<T> {
                         )),
                     );
                 }
-                _ => (), //XXX TODO
+                _ => unimplemented!(), //XXX TODO
             }
         }
     }
