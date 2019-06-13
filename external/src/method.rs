631cad395c47d4fe7fc7b9e86c094f48c689ceb6
Alex Norman <alex@x37v.info>
2019-06-12 18:41:57 -0700

    added dsp method alias

--- external/src/method.rs
+++ external/src/method.rs
@@ -2,6 +2,7 @@ use std::ffi::CString;
 
 //an alias for the methods that pure data wants for registration with its API
 pub type PdMethod = unsafe extern "C" fn();
+pub type PdDspMethod<T> = unsafe extern "C" fn(*mut T, *mut *mut puredata_sys::t_signal);
 
 pub type B<T> = unsafe extern "C" fn(*mut T);
 pub type F1<T> = unsafe extern "C" fn(*mut T, puredata_sys::t_float);
