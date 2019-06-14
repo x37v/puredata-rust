c68d8603a6dfbe3dfad2bd1f364ef8fc8664fd74
Alex Norman <alex@x37v.info>
2019-06-13 20:42:29 -0700

    type for perform trampoline

--- external/src/method.rs
+++ external/src/method.rs
@@ -3,6 +3,7 @@ use std::ffi::CString;
 //an alias for the methods that pure data wants for registration with its API
 pub type PdMethod = unsafe extern "C" fn();
 pub type PdDspMethod<T> = unsafe extern "C" fn(*mut T, *mut *mut puredata_sys::t_signal);
+pub type PdDspPerform = unsafe extern "C" fn(*mut puredata_sys::t_int) -> *mut puredata_sys::t_int;
 
 pub type B<T> = unsafe extern "C" fn(*mut T);
 pub type F1<T> = unsafe extern "C" fn(*mut T, puredata_sys::t_float);
