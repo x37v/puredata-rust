d575ddf2926f990a42b39bf0c84d9145866d928f
Alex Norman <alex@x37v.info>
2019-07-13 16:32:35 -0700

    target is now below

--- examples/hellodsp/test.sh
+++ examples/hellodsp/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cargo build && cp -f target/debug/libhellodsp.so hellodsp~.pd_linux && pd test.pd
+cargo build && cp -f ../../target/debug/libhellodsp.so hellodsp~.pd_linux && pd test.pd
 #cargo build && cp -f target/debug/libhellodsp.dylib hellodsp~.pd_darwin && open test.pd
