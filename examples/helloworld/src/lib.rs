5856d644f9d3842ce0f16a27145260c52fb9011d
Alex Norman <alex@x37v.info>
2019-07-27 13:21:38 -0700

    updating helloworld to be like the howto

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,43 +1,24 @@
 use puredata_external::builder::ControlExternalBuilder;
 use puredata_external::external::ControlExternal;
-use puredata_external::outlet::{OutletSend, OutletType};
 use puredata_external::pd;
 use puredata_external_macros::external;
 use std::ffi::CString;
-use std::ops::Deref;
-use std::rc::Rc;
 
+//based on https://github.com/pure-data/externals-howto#my-first-external-helloworld
 external! {
-    pub struct HelloWorld {
-        inlet: Rc<dyn Deref<Target = puredata_sys::t_float>>,
-        outlet: Rc<dyn OutletSend>,
-    }
+    //the struct doesn't need to keep track of anything
+    pub struct HelloWorld;
 
     impl ControlExternal for HelloWorld {
-        fn new(builder: &mut dyn ControlExternalBuilder<Self>) -> Self {
-            Self {
-                inlet: builder.new_passive_float_inlet(4f32),
-                outlet: builder.new_message_outlet(OutletType::Float),
-            }
+        fn new(_builder: &mut dyn ControlExternalBuilder<Self>) -> Self {
+            Self { }
         }
     }
 
     impl HelloWorld {
-        #[bang]
+        #[bang] //indicates that a bang in Pd should call this
         pub fn bang(&mut self) {
-            self.outlet.send_float(**self.inlet);
-        }
-
-        #[sel(defaults=1)]
-        pub fn blah(&mut self, arg: puredata_sys::t_float) {
-            self.outlet.send_float(arg);
-        }
-
-        #[sel(name = "soda", defaults=1)]
-        pub fn sel2(&mut self, arg: &mut puredata_sys::t_symbol) {
-            let m =
-                CString::new(format!("got soda {}", arg).to_string()).expect("CString::new failed");
-            pd::post(m);
+            pd::post(CString::new("Hello world !!").unwrap());
         }
     }
 }
