1348ca0ef9c7cd425125287bd86b16059bbe2268
Alex Norman <alex@x37v.info>
2019-07-27 09:48:17 -0700

    impl display for symbol so we can print it

--- puredata-sys/src/lib.rs
+++ puredata-sys/src/lib.rs
@@ -3,3 +3,18 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::fmt;
+
+impl fmt::Display for &mut crate::_symbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        unsafe {
+            let c = std::ffi::CStr::from_ptr(self.s_name);
+            if let Ok(c) = c.to_str() {
+                write!(f, "{}", c)
+            } else {
+                Err(std::fmt::Error {})
+            }
+        }
+    }
+}
