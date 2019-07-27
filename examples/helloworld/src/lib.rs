b710a7ff94e1b4b03c2bdf4472a17f2b658ffd7d
Alex Norman <alex@x37v.info>
2019-07-27 10:51:15 -0700

    using outlet

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,5 +1,6 @@
 use puredata_external::builder::ControlExternalBuilder;
 use puredata_external::external::ControlExternal;
+use puredata_external::outlet::{OutletSend, OutletType};
 use puredata_external::pd;
 use puredata_external_macros::external;
 use std::ffi::CString;
@@ -9,14 +10,14 @@ use std::rc::Rc;
 external! {
     pub struct HelloWorld {
         inlet: Rc<dyn Deref<Target = puredata_sys::t_float>>,
-        //outlet: Rc<dyn OutletSend>,
+        outlet: Rc<dyn OutletSend>,
     }
 
     impl ControlExternal for HelloWorld {
         fn new(builder: &mut dyn ControlExternalBuilder<Self>) -> Self {
             Self {
                 inlet: builder.new_passive_float_inlet(4f32),
-                //outlet: builder.new_outlet(OutletType::Float),
+                outlet: builder.new_message_outlet(OutletType::Float),
             }
         }
     }
@@ -24,16 +25,12 @@ external! {
     impl HelloWorld {
         #[bang]
         pub fn bang(&mut self) {
-            let m = CString::new(format!("hello {}", **self.inlet).to_string())
-                .expect("CString::new failed");
-            pd::post(m);
+            self.outlet.send_float(**self.inlet);
         }
 
         #[sel(defaults=1)]
         pub fn blah(&mut self, arg: puredata_sys::t_float) {
-            let m =
-                CString::new(format!("got blah {}", arg).to_string()).expect("CString::new failed");
-            pd::post(m);
+            self.outlet.send_float(arg);
         }
 
         #[sel(name = "soda", defaults=1)]
