a6356369e7aa5ae6fddb49029d0de25e73779fe2
Alex Norman <alex@x37v.info>
2019-07-28 21:23:28 -0700

    wrap the type, not the ptr

--- external/src/atom.rs
+++ external/src/atom.rs
@@ -1,15 +1,16 @@
 #[repr(transparent)]
-pub struct Atom {
-    pub inner: *const puredata_sys::t_atom,
-}
+pub struct Atom(puredata_sys::t_atom);
 
 impl Atom {
+    pub fn as_ptr(&self) -> *const puredata_sys::t_atom {
+        &self.0 as *const puredata_sys::t_atom
+    }
     pub fn get_symbol(&self) -> Option<&mut puredata_sys::t_symbol> {
-        assert!(!self.inner.is_null());
+        assert!(!self.as_ptr().is_null());
         unsafe {
-            match (*self.inner).a_type {
+            match self.0.a_type {
                 puredata_sys::t_atomtype::A_DEFSYM | puredata_sys::t_atomtype::A_SYMBOL => {
-                    let s = puredata_sys::atom_getsymbol(self.inner);
+                    let s = puredata_sys::atom_getsymbol(self.as_ptr());
                     if s.is_null() {
                         None
                     } else {
@@ -21,23 +22,23 @@ impl Atom {
         }
     }
     pub fn get_float(&self) -> Option<puredata_sys::t_float> {
-        assert!(!self.inner.is_null());
+        assert!(!self.as_ptr().is_null());
         unsafe {
-            match (*self.inner).a_type {
+            match self.0.a_type {
                 puredata_sys::t_atomtype::A_DEFFLOAT | puredata_sys::t_atomtype::A_FLOAT => {
-                    Some(puredata_sys::atom_getfloat(self.inner))
+                    Some(puredata_sys::atom_getfloat(self.as_ptr()))
                 }
                 _ => None,
             }
         }
     }
     pub fn get_int(&self) -> Option<puredata_sys::t_int> {
-        assert!(!self.inner.is_null());
+        assert!(!self.as_ptr().is_null());
         unsafe {
-            match (*self.inner).a_type {
+            match self.0.a_type {
                 //pd does the cast
                 puredata_sys::t_atomtype::A_DEFFLOAT | puredata_sys::t_atomtype::A_FLOAT => {
-                    Some(puredata_sys::atom_getint(self.inner))
+                    Some(puredata_sys::atom_getint(self.as_ptr()))
                 }
                 _ => None,
             }
