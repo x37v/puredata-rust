b0c29f10dc39b23434fab9cc01ff6444453171e8
Alex Norman <alex@x37v.info>
2019-07-28 21:43:16 -0700

    implemented counter

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -1,36 +1,34 @@
 use puredata_external::builder::ControlExternalBuilder;
 use puredata_external::external::ControlExternal;
-use puredata_external::pd;
+use puredata_external::outlet::{OutletSend, OutletType};
 use puredata_external_macros::external;
-use std::ffi::CString;
+use std::rc::Rc;
 
 //based on https://github.com/pure-data/externals-howto#a-simple-external-counter
 external! {
-    //the struct doesn't need to keep track of anything
     pub struct Counter {
+        count: isize,
+        outlet: Rc<dyn OutletSend>
     }
 
     impl ControlExternal for Counter {
         fn new(builder: &mut dyn ControlExternalBuilder<Self>) -> Self {
-            if let Some(name) = builder.instance_name() {
-                println!("name: {}", name);
-            }
-            let args = builder.creation_args();
-            println!("args len {}", args.len());
-            for a in args.iter() {
-                println!("got arg");
-                if let Some(f) = a.get_float() {
-                    println!("got arg {}", f);
-                }
-            }
-            Self { }
+            let count = if let Some(atom) = builder.creation_args().iter().next() {
+                atom.get_int().unwrap_or(0) as isize
+            } else {
+                0isize
+            };
+            let outlet = builder.new_message_outlet(OutletType::Float);
+            Self { count, outlet }
         }
     }
 
     impl Counter {
-        #[bang] //indicates that a bang in Pd should call this
+        #[bang]
         pub fn bang(&mut self) {
-            pd::post(CString::new("Hello world !!").unwrap());
+            let f = self.count as puredata_sys::t_float;
+            self.count = self.count.wrapping_add(1);
+            self.outlet.send_float(f);
         }
     }
 }
