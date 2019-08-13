367d068231ccc9b823334524351bf4d81b6ed33e
Alex Norman <alex@x37v.info>
2019-08-12 18:26:19 -0700

    supporting both dsp and control, removed shell scripts

--- macros/examples/hellodsp/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cargo build && cp -f ./target/debug/libhellodsp.so hellodsp~.pd_linux && pd test.pd
-#cargo build && cp -f target/debug/libhellodsp.dylib hellodsp~.pd_darwin && open test.pd
