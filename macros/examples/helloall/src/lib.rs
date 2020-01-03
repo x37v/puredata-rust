e1edb9f527fbf8209f8e6c9fac8a1e1256310178
Alex Norman <alex@x37v.info>
2020-01-02 20:27:16 -0800

    nicer example, basic pointer

--- macros/examples/helloall/src/lib.rs
+++ macros/examples/helloall/src/lib.rs
@@ -25,20 +25,41 @@ external! {
 
         #[list] //indicates that a list in Pd should call this
         pub fn list(&mut self, list: &[pd_ext::atom::Atom]) {
-            let s = CString::new("toast").unwrap().into();
-            self.outlet.send_anything(s, &list);
-            self.outlet.send_symbol(s);
+            if let Ok(s) = std::convert::TryFrom::try_from("list") {
+                self.outlet.send_symbol(s);
+                self.outlet.send_anything(s, &list);
+            }
         }
 
         #[anything]
         pub fn foo(&mut self, sel: pd_ext::symbol::Symbol, list: &[pd_ext::atom::Atom]) {
+            if let Ok(s) = std::convert::TryFrom::try_from("anything") {
+                self.outlet.send_symbol(s);
+            }
             self.outlet.send_symbol(sel);
             self.outlet.send_list(&list);
         }
 
         #[sel]
         pub fn bar(&mut self, s: pd_ext::symbol::Symbol) {
+            if let Ok(s) = std::convert::TryFrom::try_from("bar") {
+                self.outlet.send_symbol(s);
+            }
             self.outlet.send_symbol(s);
+        }
+
+        #[sel]
+        pub fn var(&mut self, list: &[pd_ext::atom::Atom]) {
+            if let Ok(s) = std::convert::TryFrom::try_from("sel var") {
+                self.outlet.send_symbol(s);
+            }
+            self.outlet.send_list(&list);
+        }
+
+        #[pointer]
+        pub fn baz(&mut self, p: pd_ext::pointer::Pointer) {
+            println!("got pointer");
+            //XXX
         }
     }
 }
