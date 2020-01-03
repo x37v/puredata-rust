a877dab0c37628c6d72d1044f4f63c72bcce4e1c
Alex Norman <alex@x37v.info>
2020-01-02 21:31:44 -0800

    pointer methods

--- external/src/pointer.rs
+++ external/src/pointer.rs
@@ -1,2 +1,40 @@
 #[repr(transparent)]
 pub struct Pointer(pub *mut pd_sys::t_gpointer);
+
+#[repr(transparent)]
+pub struct GList(pub *mut pd_sys::_glist);
+
+#[repr(transparent)]
+pub struct Array(pub *mut pd_sys::_array);
+
+impl Pointer {
+    pub fn check(&self, headok: bool) -> bool {
+        unsafe { pd_sys::gpointer_check(self.0, if headok { 1 } else { 0 }) != 0 }
+    }
+
+    pub fn unset(&self) {
+        unsafe {
+            pd_sys::gpointer_unset(self.0);
+        }
+    }
+
+    pub fn get_glist(&self) -> Option<GList> {
+        unsafe {
+            if (*(*self.0).gp_stub).gs_which == pd_sys::GP_GLIST as i32 {
+                Some(GList((*((*self.0).gp_stub)).gs_un.gs_glist))
+            } else {
+                None
+            }
+        }
+    }
+
+    pub fn get_array(&self) -> Option<Array> {
+        unsafe {
+            if (*(*self.0).gp_stub).gs_which == pd_sys::GP_ARRAY as i32 {
+                Some(Array((*((*self.0).gp_stub)).gs_un.gs_array))
+            } else {
+                None
+            }
+        }
+    }
+}
