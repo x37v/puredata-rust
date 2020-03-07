8b04ee2dc12102efbca885a93c7782931d5e034a
Alex Norman <alex@x37v.info>
2020-03-07 15:18:05 -0800

    update examples for split dsp and dsp control

--- macros/examples/hellodsp/src/lib.rs
+++ macros/examples/hellodsp/src/lib.rs
@@ -1,10 +1,25 @@
 use pd_ext::builder::SignalProcessorExternalBuilder;
-use pd_ext::external::SignalProcessorExternal;
+use pd_ext::external::{SignalProcessor, SignalProcessorExternal};
 use pd_ext::pd;
 
 use pd_ext_macros::external;
 
 use std::ffi::CString;
+
+struct Processor;
+
+impl SignalProcessor for Processor {
+    fn process(
+        &mut self,
+        _frames: usize,
+        inputs: &[&mut [pd_sys::t_float]],
+        outputs: &mut [&mut [pd_sys::t_float]],
+    ) {
+        for (output, input) in outputs.iter_mut().zip(inputs.iter()) {
+            output.copy_from_slice(input);
+        }
+    }
+}
 
 external! {
     pub struct HelloDSP;
@@ -39,23 +54,13 @@ external! {
     }
 
     impl SignalProcessorExternal for HelloDSP {
-        fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> Self {
+        fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> (Self, Box<dyn SignalProcessor>) {
             builder.new_signal_outlet();
             builder.new_signal_outlet();
             builder.new_signal_outlet();
             builder.new_signal_inlet();
             builder.new_signal_inlet();
-            Self { }
-        }
-        fn process(
-            &mut self,
-            _frames: usize,
-            inputs: &[&mut [pd_sys::t_float]],
-            outputs: &mut [&mut [pd_sys::t_float]],
-            ) {
-            for (output, input) in outputs.iter_mut().zip(inputs.iter()) {
-                output.copy_from_slice(input);
-            }
+            (Self { }, Box::new(Processor))
         }
     }
 }
--- macros/examples/xfade/src/lib.rs
+++ macros/examples/xfade/src/lib.rs
@@ -1,41 +1,48 @@
 use itertools::izip;
 use pd_ext::builder::SignalProcessorExternalBuilder;
-use pd_ext::external::SignalProcessorExternal;
+use pd_ext::external::{SignalProcessor, SignalProcessorExternal};
 use pd_ext_macros::external;
 use std::ops::Deref;
 
-external! {
-    //based on pan~ from: https://github.com/pure-data/externals-howto#a-signal-external-pan
-    pub struct XFade {
-        //a passive float input, the xfade position
-        pos: Box<dyn Deref<Target = pd_sys::t_float>>,
+//based on pan~ from: https://github.com/pure-data/externals-howto#a-signal-external-pan
+struct XFadeProcessor {
+    //a passive float input, the xfade position
+    pos: Box<dyn Deref<Target = pd_sys::t_float> + Send>,
+}
+
+impl SignalProcessor for XFadeProcessor {
+    //compute the audio
+    fn process(
+        &mut self,
+        _frames: usize,
+        inputs: &[&mut [pd_sys::t_float]],
+        outputs: &mut [&mut [pd_sys::t_float]],
+    ) {
+        //read the value of our position setting
+        let pos = num::clamp(**self.pos, 0f32.into(), 1f32.into());
+
+        //compute!
+        let lpan = 1f32 - pos;
+        let rpan = pos;
+        for (o, l, r) in izip!(outputs[0].iter_mut(), inputs[0].iter(), inputs[1].iter()) {
+            *o = *l * lpan + *r * rpan;
+        }
     }
+}
+
+external! {
+    pub struct XFade;
 
     impl SignalProcessorExternal for XFade {
         //build the object
-        fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> Self {
+        fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> (Self, Box<dyn SignalProcessor>) {
             //2 signal inlets (1 default)
             //1 float inlet (position)
             //1 signal outlet
             builder.new_signal_inlet();
             let pos = builder.new_passive_float_inlet(0f32);
             builder.new_signal_outlet();
-            Self {
-                pos
-            }
-        }
-
-        //compute the audio
-        fn process(&mut self, _frames: usize, inputs: &[&mut [pd_sys::t_float]], outputs: &mut [&mut [pd_sys::t_float]],) {
-            //read the value of our position setting
-            let pos = num::clamp(**self.pos, 0f32.into(), 1f32.into());
-
-            //compute!
-            let lpan = 1f32 - pos;
-            let rpan = pos;
-            for (o, l, r) in izip!(outputs[0].iter_mut(), inputs[0].iter(), inputs[1].iter()) {
-                *o = *l * lpan + *r * rpan;
-            }
+            (Self, Box::new(XFadeProcessor { pos }))
         }
     }
 }
