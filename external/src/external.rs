59047b7bc0bb85788f1505b35d28bbbe1baae791
Alex Norman <alex@x37v.info>
2020-03-07 15:17:44 -0800

    split up dsp from dsp control

--- external/src/external.rs
+++ external/src/external.rs
@@ -4,18 +4,32 @@ pub trait ControlExternal {
     fn new(builder: &mut dyn ControlExternalBuilder<Self>) -> Self;
 }
 
-pub trait SignalGeneratorExternal {
-    fn new(builder: &mut dyn SignalGeneratorExternalBuilder<Self>) -> Self;
+pub trait SignalGenerator: Send {
     fn generate(&mut self, frames: usize, outputs: &mut [&mut [pd_sys::t_float]]);
+    //called when dsp is set up, can be used to setup resources
+    fn setup_generate(&mut self, _frames: usize, _outputs: usize) {}
 }
 
-//has 1 default signal inlet
-pub trait SignalProcessorExternal {
-    fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> Self;
+pub trait SignalProcessor: Send {
     fn process(
         &mut self,
         frames: usize,
         inputs: &[&mut [pd_sys::t_float]],
         outputs: &mut [&mut [pd_sys::t_float]],
     );
+    //called when dsp is set up, can be used to setup resources
+    fn setup_process(&mut self, _frames: usize, _inputs: usize, _outputs: usize) {}
+}
+
+pub trait SignalGeneratorExternal: Sized {
+    fn new(
+        builder: &mut dyn SignalGeneratorExternalBuilder<Self>,
+    ) -> (Self, Box<dyn SignalGenerator>);
+}
+
+//has 1 default signal inlet
+pub trait SignalProcessorExternal: Sized {
+    fn new(
+        builder: &mut dyn SignalProcessorExternalBuilder<Self>,
+    ) -> (Self, Box<dyn SignalProcessor>);
 }
