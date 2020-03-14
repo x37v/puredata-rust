d8a331176475de300b9183e391b13f05bfe2d442
Alex Norman <alex@x37v.info>
2020-03-14 15:14:46 -0700

    sample rate func

--- external/src/pd.rs
+++ external/src/pd.rs
@@ -1,5 +1,9 @@
 use std::ffi::CString;
 
+pub fn sample_rate() -> pd_sys::t_float {
+    unsafe { pd_sys::sys_getsr() }
+}
+
 pub fn post(s: CString) {
     unsafe {
         pd_sys::post(s.as_ptr());
